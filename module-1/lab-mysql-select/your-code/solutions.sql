2aa59535c7c3990fb8e3716bb45348cf1a97d5a8
Nancy Ulric <nancyulric@gmail.com>
2019-07-03 20:46:16 +0200

    modif

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -8,6 +8,41 @@ JOIN publishers
 	ON publishers.pub_id = titles.pub_id
 order by au_id;
 
-SET sql_mode = '';
 SELECT COUNT(*) FROM titleauthor;
+
+SET sql_mode = '';
+
+SELECT authors.au_id, authors.au_lname, authors.au_fname, publishers.pub_name, COUNT(titles.title)
+FROM authors
+JOIN titleauthor
+	ON titleauthor.au_id = authors.au_id
+JOIN titles
+	ON titles.title_id = titleauthor.title_id
+JOIN publishers
+	ON publishers.pub_id = titles.pub_id
+GROUP BY authors.au_id, publishers.pub_name
+order by au_id DESC;
+
+SET sql_mode = '';
+
+SELECT authors.au_id, authors.au_fname, authors.au_lname, SUM(titles.ytd_sales) AS `somme`
+FROM authors
+/*JOIN titles
+	ON titles.title_id= authors.au_id*/
+JOIN titleauthor
+	ON titleauthor.au_id = authors.au_id
+JOIN titles
+	ON titleauthor.title_id = titles.title_id
+GROUP BY titleauthor.au_id
+ORDER BY `somme` DESC
+LIMIT 3;
+
+SELECT authors.au_id, authors.au_fname, authors.au_lname, SUM(titles.ytd_sales) AS `somme`
+FROM authors
+LEFT JOIN titleauthor
+	ON titleauthor.au_id = authors.au_id
+LEFT JOIN titles
+	ON titleauthor.title_id = titles.title_id
+GROUP BY titleauthor.au_id
+ORDER BY `somme` DESC;
 
