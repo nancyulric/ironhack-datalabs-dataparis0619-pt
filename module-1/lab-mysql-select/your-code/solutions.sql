1117a8fa9573e6a04cff6287e79a9d9b880efb4c
Nancy Ulric <nancyulric@gmail.com>
2019-07-03 18:21:15 +0200

    modif

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,6 +1,13 @@
-SELECT authors.au_id, authors.au_lname, authors.au_lname, titles.title
+SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, publishers.pub_name
 FROM authors
 INNER JOIN titleauthor
 	ON titleauthor.au_id = authors.au_id
 JOIN titles
-	ON titles.title_id = titleauthor.title_id;
+	ON titles.title_id = titleauthor.title_id
+JOIN publishers
+	ON publishers.pub_id = titles.pub_id
+order by au_id;
+
+SET sql_mode = '';
+SELECT COUNT(*) FROM titleauthor;
+
